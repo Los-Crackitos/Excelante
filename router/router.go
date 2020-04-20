f3ba286c4a9b44bf460517abb576097b9484825b
Thyrael <robin.baraffe@gmail.com>
2020-04-20 10:59:57 +0200

    Add excelante router

---
+++ router/router.go
@@ -0,0 +1,32 @@
+package router
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+// CreateRouter ...
+func CreateRouter() {
+	router := mux.NewRouter()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	c := cors.New(cors.Options{
+		AllowedMethods: []string{
+			http.MethodPost,
+		},
+	})
+
+	handler := c.Handler(router)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), handler); err != nil {
+		log.Print(err)
+	}
+}
