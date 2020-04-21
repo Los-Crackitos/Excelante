5824bcb133aa417cf44b2b98ac9e64216df30c0a
Thyrael <robin.baraffe@gmail.com>
2020-04-21 09:22:05 +0200

    Fix gofmt

--- models/Item.go
+++ models/Item.go
@@ -2,7 +2,7 @@ package models
 
 // Item represent json model Item
 type Item struct {
-	Mode                    string  `json:"mode"`
-	StartingCellCoordonates string  `json:"starting_cell_coordonates,omitempty"`
+	Mode                    string        `json:"mode"`
+	StartingCellCoordonates string        `json:"starting_cell_coordonates,omitempty"`
 	Data                    []interface{} `json:"data"`
 }
