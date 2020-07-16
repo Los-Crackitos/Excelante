9d20fd60332a056e29d864fa9b28cae5bea5112c
Thyrael <robin.baraffe@gmail.com>
2020-07-16 10:59:33 +0200

    Clean code

--- models/item.go
+++ models/item.go
@@ -2,7 +2,7 @@ package models
 
 // Item struct is a thing (table, graph, ...) you want to insert into your Excel file
 type Item struct {
-	StartingCellCoordinates  string   `json:"starting_cell_coordinates,omitempty"`
-	Tables                   []*Table `json:"tables"`
-	Graph                    *Graph   `json:"graph,omitempty"`
+	StartingCellCoordinates string   `json:"starting_cell_coordinates,omitempty"`
+	Tables                  []*Table `json:"tables"`
+	Graph                   *Graph   `json:"graph,omitempty"`
 }
